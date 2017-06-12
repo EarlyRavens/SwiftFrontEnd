c35a84d9f7d6cd7da1f1376940cb5a2b7fa8ac03
Max Peiros <mpeiros@gmail.com>
2017-06-11 23:48:06 -0700

    Add table view

--- EarlyBird/ViewController.swift
+++ EarlyBird/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names = ["Max", "Mina", "Patrick", "Earl"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        if searchTextField.text != "" && locationTextField.text != "" {
+            let search = searchTextField.text!
+            let location = locationTextField.text!
+            
+            print(search)
+            print(location)
+        }
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell!
+        
+        cell!.textLabel!.text = names[indexPath.row]
+        
+        return cell!
+    }
 }
 
