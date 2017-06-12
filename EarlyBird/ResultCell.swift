60c1f8f42bf9c8ae2e60fc3e2f2c94e99c75c70e
Max Peiros <mpeiros@gmail.com>
2017-06-12 15:58:11 -0700

    Update result cell view and storyboard

--- EarlyBird/ResultCell.swift
+++ EarlyBird/ResultCell.swift
@@ -13,6 +13,14 @@ class ResultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var urlLabel: UILabel!
     
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+    }
+    
     func configureCell(result: Result) {
         nameLabel.text = result.resultName
         urlLabel.text = result.resultUrl
