3c7df3890b639728f83ecfeb4018f618d7fd4a91
Max Peiros <mpeiros@gmail.com>
2017-06-12 11:40:58 -0700

    Add Alamofire

--- EarlyBird/SearchViewController.swift
+++ EarlyBird/SearchViewController.swift
@@ -21,10 +21,23 @@ class SearchViewController: UIViewController {
         self.title = "EarlyBird"
     }
 
+    func getResults(business: String, location: String) {
+        let path = "https://enigmatic-forest-54143.herokuapp.com/?business=\(business)&location=\(location)"
+        let url = URL(string: path)
+        let request = URLRequest(url: url!)
+        let session = URLSession.shared
+        _ = session.dataTask(with: request) { (data, response, error) -> Void in
+            print(data ?? "no data")
+            print(response ?? "no response")
+        }
+    }
+    
     @IBAction func searchButtonPressed(_ sender: Any) {
         if searchTextField.text != "" && locationTextField.text != "" {
-            let result = Result.init(name: "Sushi", url: "sushi.com")
+            let result = Result.init(name: "Sushi", url: "94105")
             let results = [result]
+            
+            getResults(business: result.resultName, location: result.resultUrl)
             
             performSegue(withIdentifier: "ShowResults", sender: results)
         }
